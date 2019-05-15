284516bcfbb673ff63231520be80e22920f79280
edwardreeve <edreeve+git@gmail.com>
2019-05-15 17:23:31 +0100

    Chapter 18 in progress

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -5,4 +5,10 @@ describe Bike do
         bike = Bike.new
         expect(bike).to respond_to(:working?)
     end
+
+    it 'can be set to broken' do
+        bike = Bike.new
+        bike.broken
+        expect(bike.working?).to eq(false)
+    end
 end
